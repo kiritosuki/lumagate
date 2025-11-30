486f77885a530755c8d5b9184c0d8707938f52c3
kirito <3322640054@qq.com>
2025-11-30 15:39:33 +0800

    部署

--- gateway/state.go
+++ gateway/state.go
@@ -67,14 +67,14 @@ func initState() {
 		ID:     "users",
 		Prefix: "/api/users",
 		V1: []Upstream{
-			{Name: "A", URL: "http://localhost:9001", Weight: 1},
-			{Name: "B", URL: "http://localhost:9002", Weight: 1},
-			{Name: "C", URL: "http://localhost:9003", Weight: 1},
+			{Name: "A", URL: "http://serviceA_v1:9001", Weight: 1},
+			{Name: "B", URL: "http://serviceB_v1:9002", Weight: 1},
+			{Name: "C", URL: "http://serviceC_v1:9003", Weight: 1},
 		},
 		V2: []Upstream{
-			{Name: "A", URL: "http://localhost:9011", Weight: 1},
-			{Name: "B", URL: "http://localhost:9012", Weight: 1},
-			{Name: "C", URL: "http://localhost:9013", Weight: 1},
+			{Name: "A", URL: "http://serviceA_v2:9011", Weight: 1},
+			{Name: "B", URL: "http://serviceB_v2:9012", Weight: 1},
+			{Name: "C", URL: "http://serviceC_v2:9013", Weight: 1},
 		},
 		LBEnabled: false,
 		Plugins: Plugins{
